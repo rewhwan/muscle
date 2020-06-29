a91c89e5a9da98becad24f6ac2a3d72fcaccc0ac
Kim Jinhyeok <derrick11@naver.com>
2020-06-29 10:46:12 +0900

    공지사항 테이블 및 답변 등록 UI 및 기능 추가

---
+++ src/controller/AnswerDAO.java
@@ -0,0 +1,64 @@
+package controller;
+
+import model.Member;
+import model.QuestionMember;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class AnswerDAO {
+
+    //DB연결에 필요한 것들 전역변수로 선언
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    //쿼리문 결과값 반환을 위한 전역변수 선언
+    ArrayList<Member> arrayList = null;
+    int returnValue = 0;
+
+    public int answerRegist (QuestionMember question , String contents, Member memberLogin) {
+        try {
+            con = DBUtill.getConnection();
+
+            if(con != null) {
+                System.out.println("AnswerDAO.answerRegist : DB 연결 성공");
+            }else {
+                System.out.println("AnswerDAO.answerRegist : DB 연결 실패");
+            }
+
+            //쿼리문
+            String query = "INSERT INTO answer VALUES(?,?,?,now())";
+
+            pstmt = con.prepareStatement(query);
+
+            pstmt.setInt(1,question.getNo());
+            pstmt.setString(2, contents);
+            pstmt.setString(3,memberLogin.getId());
+
+            returnValue = pstmt.executeUpdate();
+
+            if(returnValue != 0) {
+                AlertUtill.showInformationAlert("답변등록 성공","답변등록에 성공하였습니다.","확인을 누르면 로그인 화면으로 돌아갑니다.");
+            }else {
+                throw new Exception(question.getNo()+"번 답변 등록 문제 있음");
+            }
+        } catch (Exception e) {
+            // 경고창이 만들어짐
+            AlertUtill.showWarningAlert("답변등록 실패","답변등록에 실패하였습니다.","문제사항 : "+e.getMessage());
+        } finally {
+            try {
+                if(pstmt != null)pstmt.close();
+                if(con != null)con.close();
+
+            } catch (SQLException e) {
+                System.out.println("AnswerDAO.answerRegist "+e.getMessage());
+            }
+        }
+
+        return returnValue;
+    }
+}
--- src/controller/AnswerRegistController.java
+++ src/controller/AnswerRegistController.java
@@ -1,12 +1,38 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import model.Member;
+import model.QuestionMember;
 
-import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AnswerRegistController {
+public class AnswerRegistController implements Initializable {
 
-    //버튼
-    @FXML Button btnRegist;
-    @FXML Button btnExit;
+
+
+    //기본 스테이지 변수
+    public Stage primarystage;
+
+    //질문 객체 저장
+    public QuestionMember question;
+
+    //로그인 정보 객체 저장
+    public Member memberLogin;
+
+    //DB에 작업을 도와주는 변수들
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+
+    }
+
+
 }
